cd33dbb6247d11f85ccdd7e15b9bf2e61dca9ed0
a9261020 <fish840212@yahoo.com.tw>
2025-03-21 18:31:49 +0800

    update

---
+++ app/Hi.tsx
@@ -0,0 +1,7 @@
+const Hi = () => {
+    return (
+        <p>Hi</p>
+    )
+}
+
+export default Hi;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Hi from "./Hi";
+
 export default function Home() {
   return (
     <main>
+      <Hi />
       <div>Next.js on GitHub Pages</div>
     </main>
   );
